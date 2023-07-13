c8ebe41da9906d7f18b645c79bca57d497a2fb73
AshiqTasdid <admin@ashiqthedev.com>
2023-07-13 17:57:22 +0600

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Space_Grotesk } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Space_Grotesk({ subsets: ['latin'] })
 
@@ -16,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={inter.className}>{children}</body>
     </html>
   )
